bcde67e5050df20456be8b645d6c1fa7246da004
dzimentey <dzimentey@gmail.com>
2022-03-08 16:43:11 +0300

    refactor: delete unused code

--- my-app/src/Counter.tsx
+++ my-app/src/Counter.tsx
@@ -23,8 +23,6 @@ export const Counter =()=> {
             <div>{count}</div>
             <input type ="number" value = {value} onChange ={(e)=>{setValue(e.currentTarget.valueAsNumber)}}/>
 
-            <h1>Hello CodeSandbox</h1>
-            <h2>Start editing to see some magic happen!</h2>
         </div>
     );
 
